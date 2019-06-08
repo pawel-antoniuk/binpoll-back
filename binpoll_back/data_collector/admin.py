ee54d29865ed2c73cd51b54ce245ae5722445923
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-06-08 15:14:29 +0200

    comments handling

--- binpoll_back/data_collector/admin.py
+++ binpoll_back/data_collector/admin.py
@@ -4,5 +4,6 @@ from . import models
 admin.site.register(models.PollData)
 admin.site.register(models.AudioSample)
 admin.site.register(models.AudioSet)
+admin.site.register(models.Comment)
 
 # Register your models here.
