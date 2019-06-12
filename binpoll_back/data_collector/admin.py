51cf9b652f231c213115532e40db4c0b2ed62cd9
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-06-12 13:26:34 +0200

    update admin panel

--- binpoll_back/data_collector/admin.py
+++ binpoll_back/data_collector/admin.py
@@ -4,6 +4,8 @@ from . import models
 admin.site.register(models.PollData)
 admin.site.register(models.AudioSample)
 admin.site.register(models.AudioSet)
+admin.site.register(models.UserInfo)
+admin.site.register(models.Problem)
 admin.site.register(models.Comment)
 
 # Register your models here.
